22fb55de2468ea6e7937793581abbe60a2a8fe1a
Eudgene <Eudgene8@mail.ru>
2021-07-13 08:17:34 +0300

    add some code for example

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-//const { Command } = require('../');
-//const program = new Command();
-console.log('checred!');
+const { Command } = require('../');
+const program = new Command();
+program
+    .description('example')
+    .version('0.0.1');
+
+program.parse();
