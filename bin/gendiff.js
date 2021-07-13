c456dd708b922e1983cdd0fe3a98c1224f799a4b
Eudgene <Eudgene8@mail.ru>
2021-07-13 08:18:33 +0300

    add in gandiff.js console.log to checked

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-const { Command } = require('../');
+/*const { Command } = require('../');
 const program = new Command();
 program
     .description('example')
     .version('0.0.1');
 
-program.parse();
+program.parse();*/
+console.log('hdhdhd');
