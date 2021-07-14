741b1709efeb4b28a76e104f8c045807d8390ab5
Eudgene <Eudgene8@mail.ru>
2021-07-14 08:41:36 +0300

    add .arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,6 @@ program
     .version('0.0.1', '-V, --version', 'output the version number')
     .argument('<filepath1>', 'path to file1')
     .argument('<filepath2>', 'path to file2')
-    .argument('<username>', 'user to login')
-    .argument('[password]', 'password for user, if required', 'no password given')
     .option('-f, --format [type]', 'output format');
 
 program.parse();
