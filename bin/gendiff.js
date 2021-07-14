3bb7cf7028b76e28acbc004a320d51d852014ee4
Eudgene <Eudgene8@mail.ru>
2021-07-14 08:44:25 +0300

    add .arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,8 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .argument('<filepath1>', 'path to file1')
-    .argument('<filepath2>', 'path to file2')
-    .option('-f, --format [type]', 'output format');
+    //.argument('<filepath1>', 'path to file1')
+    //.argument('<filepath2>', 'path to file2')
+    .option('-f, --format [type]', 'output format', '<filepath2>', '<filepath1>');
 
 program.parse();
