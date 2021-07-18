4f780245b7df39b6f98beebd40dd4f947d15212b
Eudgene <Eudgene8@mail.ru>
2021-07-18 12:24:55 +0300

    add .action without code

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,9 @@ program
     .version('0.0.1', '-V, --version', 'output the version number')
     .argument('<filepath1>', 'path to file1')
     .argument('<filepath2>', 'path to file2')
-    .option('-f, --format [type]', 'output format');
+    .option('-f, --format [type]', 'output format')
+    .action((file1, file2) => {
+
+    });
 
 program.parse();
