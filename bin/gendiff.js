cebb652e385c7ffee458edc6d0cefd593e829f81
Eudgene <Eudgene8@mail.ru>
2021-08-02 08:08:28 +0300

    add isAbsOrNot to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -16,7 +16,7 @@ program
         //const _ = require('lodash');
         //console.log(filepath1, filepath2);
         //const path = require('path');
-        console.log(typeof filepath1);
+        console.log(filepath1);
         const takeObjectFromJson = (file) => {
           const read = fs.readFileSync(file, 'utf8');
           const readJson = JSON.parse(read);
