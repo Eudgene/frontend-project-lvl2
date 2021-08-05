4a71828b2fde7ffb1c3dc72127b87d12478ecb2d
Eudgene <Eudgene8@mail.ru>
2021-08-05 08:27:15 +0300

    change .action  to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -13,7 +13,8 @@ program
     .option('-f, --format [type]', 'output format')
     .action((filepath1, filepath2) => {
       const takeObjectFromJson = (file) => {
-        const filePath = path.isAbsolute(file) ? path.resolve(file) : process.cwd();
+        //const filePath = path.isAbsolute(file) ? path.resolve(file) : process.cwd();
+        const filePath = process.cwd();
         const read = fs.readFileSync(`${filePath}/${file}`, 'utf8');
         const readJson = JSON.parse(read);
         return readJson;
