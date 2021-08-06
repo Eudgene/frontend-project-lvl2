792e770a8b12e96589d37430d519a7a93f671e0e
Eudgene <Eudgene8@mail.ru>
2021-08-06 08:44:10 +0300

    change .action  to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@
 import { Command } from 'commander/esm.mjs';
 import * as fs from 'fs';
 import * as _ from 'lodash';
+import { uniq } from 'lodash';
 import * as path from 'path';
 import { cwd } from 'process';
 const program = new Command();
@@ -22,7 +23,7 @@ program
       const json2 = takeObjectFromJson(filepath2);
       const arr = Object.keys(json1);
       const arr2 = Object.keys(json2);
-      const commonArr = _.uniq(arr.concat(arr2).sort());
+      const commonArr = uniq(arr.concat(arr2).sort());
       const newResd = commonArr.map((item) => {
         if (arr2.includes(item)) {
           if (json1[item] === json2[item]) {
