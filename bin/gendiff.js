f26ffe09d36c2ddcf8ca3c082a1aea6fe36919ea
Eudgene <Eudgene8@mail.ru>
2021-08-06 08:21:41 +0300

    change .action  to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .argument('<filepath1> <filepath2>')
+    .arguments('<filepath1> <filepath2>')
     .option('-f, --format [type]', 'output format')
     .action((filepath1, filepath2) => {
       const takeObjectFromJson = (file) => {
