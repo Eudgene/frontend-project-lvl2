c9f24749f234ab5a911e146ad181af9db26453c1
Eudgene <Eudgene8@mail.ru>
2021-08-06 08:23:40 +0300

    change .action  to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -25,7 +25,7 @@ program
       const json2 = takeObjectFromJson(filepath2);
       const arr = Object.keys(json1);
       const arr2 = Object.keys(json2);
-      const commonArr = _.uniq(_.concat(arr, arr2).sort());
+      const commonArr = _.uniq(arr.concat(arr2).sort());
       const newResd = commonArr.map((item) => {
         if (arr2.includes(item)) {
           if (json1[item] === json2[item]) {
