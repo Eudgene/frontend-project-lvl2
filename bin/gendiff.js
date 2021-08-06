8230ae9f2045a9fbfc9d664ba5bd72c816fe8a50
Eudgene <Eudgene8@mail.ru>
2021-08-06 08:50:21 +0300

    change .action  to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import * as fs from 'fs';
 //import * as _ from 'lodash';
 //import pkg from 'lodash';
 //const { uniq } = pkg;
-import uniq from 'lodash/array/uniq';
+var _ = require('lodash');
 import * as path from 'path';
 import { cwd } from 'process';
 const program = new Command();
@@ -25,7 +25,7 @@ program
       const json2 = takeObjectFromJson(filepath2);
       const arr = Object.keys(json1);
       const arr2 = Object.keys(json2);
-      const commonArr = uniq(arr.concat(arr2).sort());
+      const commonArr = _.uniq(arr.concat(arr2).sort());
       const newResd = commonArr.map((item) => {
         if (arr2.includes(item)) {
           if (json1[item] === json2[item]) {
