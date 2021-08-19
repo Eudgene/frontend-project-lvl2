1b0d08f2d7cc8cdf8beb8bad85640ee055d06e54
Eudgene <Eudgene8@mail.ru>
2021-08-19 08:35:05 +0300

    add two tests 1v

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -1,5 +1,21 @@
-import action from '../bin/gendiff.js';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+import { dirname } from 'path';
+import * as fs from 'fs';
+import { takeObjectFromJson } from '../index.js';
 
-test('action', () => {
-    expect(action(typeof filepath1)).toEqual(string);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', 'bin', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('file type', () => {
+  const res = readFile('file1.json');
+  expect(typeof res).toEqual('string');
+})
+
+test('takeObjectFromJson result type', () => {
+  const obj = takeObjectFromJson('file1.json');
+  expect(typeof obj).toEqual('object');
 })
