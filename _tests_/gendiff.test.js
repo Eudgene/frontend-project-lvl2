a41696f4e09fc6567bb82cf324440722d0676ebc
Eudgene <Eudgene8@mail.ru>
2021-08-20 08:21:38 +0300

    changed anderskore

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -4,10 +4,10 @@ import { dirname } from 'path';
 import * as fs from 'fs';
 import * as func from '../bin/gendiff';
 
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = dirname(_filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(_dirname, '..', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('file type', () => {
