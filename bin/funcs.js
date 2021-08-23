0ace029ab87eb01aa390616c0ce54bd895ff0dc5
Eudgene <Eudgene8@mail.ru>
2021-08-23 08:53:59 +0300

    changed after tests

--- bin/funcs.js
+++ bin/funcs.js
@@ -2,10 +2,10 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const takeObjectFromJson = (file) => {
-    const filePath = path.isAbsolute(file) ? process.cwd() : path.resolve(file);
-    const read = fs.readFileSync(filePath, 'utf8');
-    const readJson = JSON.parse(read);
-    return readJson;
+  const filePath = path.isAbsolute(file) ? process.cwd() : path.resolve(file);
+  const read = fs.readFileSync(filePath, 'utf8');
+  const readJson = JSON.parse(read);
+  return readJson;
 };
 
 export default takeObjectFromJson;
