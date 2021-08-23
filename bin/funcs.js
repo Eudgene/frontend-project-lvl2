58fff89c0714c31eabdde20d75527345a46a309e
Eudgene <Eudgene8@mail.ru>
2021-08-23 08:41:31 +0300

    add new file to funcs

--- bin/funcs.js
+++ bin/funcs.js
@@ -1,3 +1,6 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 const takeObjectFromJson = (file) => {
     const filePath = path.isAbsolute(file) ? process.cwd() : path.resolve(file);
     const read = fs.readFileSync(filePath, 'utf8');
