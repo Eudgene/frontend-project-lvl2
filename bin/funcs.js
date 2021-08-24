a598994da34af9125241612c2f2e34967cda69df
Eudgene <Eudgene8@mail.ru>
2021-08-24 08:53:14 +0300

    changed test

--- bin/funcs.js
+++ bin/funcs.js
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const takeObjectFromJson = (file) => {
-  const filePath = path.isAbsolute(file) ? file : path.resolve(process.cwd(),file);
+  const filePath = path.isAbsolute(file) ? file : path.resolve(process.cwd(), file);
   const read = fs.readFileSync(`${filePath}`, 'utf8');
   const readJson = JSON.parse(read);
   return readJson;
