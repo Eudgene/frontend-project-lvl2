b43c397806309eafa2855fda6452ee2fbeb73b95
Eudgene <Eudgene8@mail.ru>
2021-08-25 08:14:50 +0300

    add new test

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -9,6 +9,12 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const example1 = {
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+};
 
 test('file type', () => {
   const res = readFile('file1.json');
@@ -19,7 +25,9 @@ test('takeObjectFromJson result type', () => {
   const obj = takeObjectFromJson('file1.json');
   const obj2 = takeObjectFromJson('file2.json');
   const obj3 = takeObjectFromJson('/mnt/c/Users/user/Desktop/Програмирование/Тестовые по Хекслет/frontend-project-lvl2/file1.json');
+  const obj4 = takeObjectFromJson('file1.json');
   expect(typeof obj).toEqual('object');
   expect(typeof obj2).toEqual('object');
   expect(typeof obj3).toEqual('object');
+  expect(obj4).toEqual(example1);
 });
