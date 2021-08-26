f5f1ecd46d3f4d670d4e2e9d3e05e1a56d0f4f26
Eudgene <Eudgene8@mail.ru>
2021-08-26 08:48:15 +0300

    changed gendiff.js with newResd

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@
 import { Command } from 'commander';
 import _ from 'lodash';
 import takeObjectFromJson from '../bin/funcs.js';
+import newResd from '../bin/funcs.js';
 
 const program = new Command();
 program
@@ -16,19 +17,10 @@ program
     const arr = Object.keys(json1);
     const arr2 = Object.keys(json2);
     const commonArr = _.uniq(arr.concat(arr2).sort());
+    const finishedArray = [];
+    const newArr = newResd(commonArr, finishedArray);
     console.log('{');
-    commonArr.forEach((item) => {
-      if (arr2.includes(item)) {
-        if (json1[item] === json2[item]) {
-          console.log(`  ${item}: ${json1[item]}`);
-        } else {
-          const b = json1[item] ? `- ${item}: ${json1[item]}\n+ ${item}: ${json2[item]}` : `+ ${item}: ${json2[item]}`;
-          console.log(b);
-        }
-      } else {
-        console.log(`- ${item}: ${json1[item]}`);
-      }
-    });
+    newArr.map((item) => console.log(item));
     console.log('}');
   });
 
