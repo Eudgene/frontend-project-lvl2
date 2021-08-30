cf9583a4d9153ca0c1663698b4959f7e1bffbeb5
Eudgene <Eudgene8@mail.ru>
2021-08-30 08:30:51 +0300

    check type of file

--- bin/funcs.js
+++ bin/funcs.js
@@ -7,6 +7,7 @@ const takeObjectFromJson = (file) => {
   const filePath = path.isAbsolute(file) ? file : path.resolve(process.cwd(), file);
   const read = fs.readFileSync(`${filePath}`, 'utf8');
   const readJson = JSON.parse(read);
+  console.log(readJson);
   return readJson;
 };
 
