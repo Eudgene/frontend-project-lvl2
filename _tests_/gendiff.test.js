2708c731972e3a7b1799c80a36fce2a673aaeb77
Eudgene <Eudgene8@mail.ru>
2021-08-30 08:28:34 +0300

    delete some test

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -22,10 +22,10 @@ test('file type', () => {
 });
 
 test('takeObjectFromJson result type', () => {
-  const obj = takeObjectFromJson('file1.json');
+  //const obj = takeObjectFromJson('file1.json');
   //const obj2 = takeObjectFromJson('file2.json');
   //const obj3 = takeObjectFromJson('/mnt/c/Users/user/Desktop/Програмирование/Тестовые по Хекслет/frontend-project-lvl2/file1.json');
-  expect(typeof obj).toEqual('object');
+  //expect(typeof obj).toEqual('object');
   //expect(typeof obj2).toEqual('object');
   //expect(typeof obj3).toEqual('object');
   expect(obj).toEqual(example1);
