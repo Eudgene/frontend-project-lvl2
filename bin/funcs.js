f0c87253dea3dc53f90d79b19ee55350ec080058
Eudgene <Eudgene8@mail.ru>
2021-08-31 08:36:25 +0300

    checked filePath

--- bin/funcs.js
+++ bin/funcs.js
@@ -4,8 +4,6 @@ import _ from 'lodash';
 
 export const takeObjectFromJson = (file) => {
   const filePath = path.isAbsolute(file) ? file : path.resolve(process.cwd(), file);
-  console.log(process.cwd());
-  console.log(filePath);
   const read = fs.readFileSync(`${filePath}`, 'utf8');
   const readJson = JSON.parse(read);
   return readJson;
