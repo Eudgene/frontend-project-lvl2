9fb38f69ddd444fa35c9156c93f8fdec11b135f7
Eudgene <Eudgene8@mail.ru>
2021-09-08 08:33:11 +0300

    changed after checked linter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { newResd } from '../bin/funcs.js';
+import { newResd } from './funcs.js';
 
 const program = new Command();
 program
