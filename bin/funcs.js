4b59fa86ddde7a55ab92602499a6995d431a6f03
Eudgene <Eudgene8@mail.ru>
2021-09-22 08:36:21 +0300

    add goOnTree

--- bin/funcs.js
+++ bin/funcs.js
@@ -39,7 +39,7 @@ export const newResd = (filepath1, filepath2) => {
   //const arr2 = goOnTree(json2);
   const arr = Object.keys(json1);
   const arr2 = Object.keys(json2);
-  console.log(json1);
+  console.log(goOnTree(json1));
   const commonArr = _.uniq(arr.concat(arr2).sort());
   
   const finishedArray = [];
