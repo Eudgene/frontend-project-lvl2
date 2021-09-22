a79aba104857deff066a18bf6b26239cc9a582aa
Eudgene <Eudgene8@mail.ru>
2021-09-22 08:13:17 +0300

    add goOnTree

--- bin/funcs.js
+++ bin/funcs.js
@@ -3,6 +3,28 @@ import * as path from 'path';
 import _ from 'lodash';
 import parsFunc from './parsers.js';
 
+const isObject = (obj) => {
+  const type = typeof obj;
+  if(type === 'object') {
+    return true;
+  }
+  return false;
+};
+
+const result = [];
+const goOnTree = (tree) => {
+  const keys = Object.keys(tree)
+    .map((item) => {
+    if(isObject(tree[item])) {
+      result.push(`${item}`);
+      goOnTree(tree[item]);
+    } else {
+      result.push(`${item}: ${tree[item]}`);
+    }
+  });
+  return result;
+};
+
 export const takeObjectFromJson = (file) => {
   const filePath = path.isAbsolute(file) ? file : path.resolve(process.cwd(), file);
   const read = fs.readFileSync(`${filePath}`, 'utf8');
@@ -13,8 +35,8 @@ export const takeObjectFromJson = (file) => {
 export const newResd = (filepath1, filepath2) => {
   const json1 = takeObjectFromJson(filepath1);
   const json2 = takeObjectFromJson(filepath2);
-  const arr = Object.keys(json1);
-  const arr2 = Object.keys(json2);
+  const arr = goOnTree(json1);
+  const arr2 = goOnTree(json2);
   const commonArr = _.uniq(arr.concat(arr2).sort());
   const finishedArray = [];
   commonArr.map((item) => {
