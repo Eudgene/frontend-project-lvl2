0728fd677815133478f17f97ac897836039500e0
Eudgene <Eudgene8@mail.ru>
2021-10-28 17:31:45 +0300

    added stylish.js with kode

---
+++ bin/stylish.js
@@ -0,0 +1,22 @@
+const stylish = (value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, depth) => {
+    if (typeof currentValue !== 'object') {
+      return currentValue.toString();
+    }
+  
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(value, 1);
+};
