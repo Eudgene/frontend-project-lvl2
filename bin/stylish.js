03ffec581b712b246e8f7a2568b9c155955853c3
Eudgene <Eudgene8@mail.ru>
2021-11-04 17:26:09 +0300

    changed stylish

--- bin/stylish.js
+++ bin/stylish.js
@@ -1,31 +1,27 @@
 import _ from 'lodash';
 
-const stylish = (data, elem = ' ', acc = 1) => {
-  const newKeys = [];
-  const str = `${data}`;
-  if (_.isPlainObject(data)) {
-    newKeys.push('{');
-    let velue = 0;
-    const keyBypass = (someKey, element, accumulator) => {
-      velue += accumulator;
-      Object.keys(someKey)
-        .map((key) => {
-          if (_.isPlainObject(someKey[key])) {
-            newKeys.push(`${element.repeat(velue)}${key}: {`);
-            keyBypass(someKey[key], element, accumulator);
-            newKeys.push(`${element.repeat(velue -= accumulator)}}`);
-          } else {
-            newKeys.push(`${element.repeat(velue)}${key}: ${someKey[key]}`);
-          }
-          return newKeys;
-        });
-      return newKeys;
-    };
-    keyBypass(data, elem, acc);
-    newKeys.push('}');
-    return newKeys.join('\n');
-  }
-  return str;
+const stylish = (value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, depth) => {
+    if (typeof currentValue !== 'object') {
+      return currentValue.toString();
+    } else if (currentValue === null) {
+      return 'null';
+    }
+    console.log(currentValue);
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 2)}`);
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(value, 1);
 };
 
 export default stylish;
