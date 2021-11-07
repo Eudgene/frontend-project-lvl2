3f462df5c731c955c865b9f12265d56312a2e425
Eudgene <Eudgene8@mail.ru>
2021-11-07 12:30:26 +0300

    changed newResd

--- bin/funcs.js
+++ bin/funcs.js
@@ -14,36 +14,39 @@ export const newResd = (tree, tree1) => {
   const keys1 = Object.keys(tree1);
   const keys = Object.keys(tree);
   const finishedArray = {};
-  const commonArr = _.uniq(keys.concat(keys1).sort());
-  commonArr.forEach((item) => {
-    if (keys1.includes(item)) {
-      if (_.isPlainObject(tree1[item])) {
-        if (tree[item]) {
-          const newItem = `  ${item}`;
-          finishedArray[newItem] = newResd(tree[item], tree1[item]);
+  const commonArr = _.uniq(keys.concat(keys1).sort())
+    .map((item) => {
+      if(keys1.includes(item)) {
+        if(_.isPlainObject(tree1[item])) {
+          if(tree[item]) {
+            const newItem = `  `;
+            finishedArray[item] = [ newItem, goOnTree1(tree[item], tree1[item]) ];
+          } else {
+            const newItem1 = `+ `;
+            finishedArray[item] = [ newItem1, tree1[item] ];
+          }
         } else {
-          const newItem1 = `+ ${item}`;
-          finishedArray[newItem1] = tree1[item];
+          if (tree[item] === tree1[item]) {
+            const newItem = `  `;
+            finishedArray[item] = [ newItem, tree[item]];
+          } else if(keys.includes(item)) {
+            const newItem = `- `;
+            const newItem1 = `+ `;
+            finishedArray[item] = [newItem, newItem1, tree[item], tree1[item]];
+          } else {
+            const newItem1 = `+ `;
+            finishedArray[item] = [newItem1, tree1[item]];
+          }
         }
-      } else if (tree[item] === tree1[item]) {
-        const newItem = `  ${item}`;
-        finishedArray[newItem] = tree[item];
-      } else if (keys.includes(item)) {
-        const newItem = `- ${item}`;
-        const newItem1 = `+ ${item}`;
-        finishedArray[newItem] = tree[item];
-        finishedArray[newItem1] = tree1[item];
       } else {
-        const newItem1 = `+ ${item}`;
-        finishedArray[newItem1] = tree1[item];
+        if(_.isPlainObject(tree[item])) {
+          const newItem = `- `;
+          finishedArray[item] = [newItem, tree[item]];
+        } else {
+          const newItem1 = `- `;
+          finishedArray[item] = [newItem1, tree[item]];
+        }
       }
-    } else if (_.isPlainObject(tree[item])) {
-      const newItem = `- ${item}`;
-      finishedArray[newItem] = tree[item];
-    } else {
-      const newItem1 = `- ${item}`;
-      finishedArray[newItem1] = tree[item];
-    }
   });
   return finishedArray;
 };
