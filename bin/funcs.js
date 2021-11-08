a372087f488fd09806cfe82ca7ba7fd839498a23
Eudgene <Eudgene8@mail.ru>
2021-11-08 17:26:41 +0300

    changed after linter

--- bin/funcs.js
+++ bin/funcs.js
@@ -14,11 +14,11 @@ export const newResd = (tree, tree1) => {
   const keys1 = Object.keys(tree1);
   const keys = Object.keys(tree);
   const finishedArray = {};
-  const commonArr = _.uniq(keys.concat(keys1).sort())
+  _.uniq(keys.concat(keys1).sort())
     .map((item) => {
-      if(keys1.includes(item)) {
-        if(_.isPlainObject(tree1[item])) {
-          if(tree[item]) {
+      if (keys1.includes(item)) {
+        if (_.isPlainObject(tree1[item])) {
+          if (tree[item]) {
             const newItem = `  `;
             finishedArray[item] = [ newItem, newResd(tree[item], tree1[item]) ];
           } else {
@@ -29,7 +29,7 @@ export const newResd = (tree, tree1) => {
           if (tree[item] === tree1[item]) {
             const newItem = `  `;
             finishedArray[item] = [ newItem, tree[item]];
-          } else if(keys.includes(item)) {
+          } else if (keys.includes(item)) {
             const newItem = `- `;
             const newItem1 = `+ `;
             finishedArray[item] = [newItem, newItem1, tree[item], tree1[item]];
@@ -39,7 +39,7 @@ export const newResd = (tree, tree1) => {
           }
         }
       } else {
-        if(_.isPlainObject(tree[item])) {
+        if (_.isPlainObject(tree[item])) {
           const newItem = `- `;
           finishedArray[item] = [newItem, tree[item]];
         } else {
