3bf45de2ccc4adbcf3e9f86474a0928e5fdf4580
Eudgene <Eudgene8@mail.ru>
2021-11-08 18:39:30 +0300

    changed after linter

--- bin/funcs.js
+++ bin/funcs.js
@@ -14,34 +14,35 @@ export const newResd = (tree, tree1) => {
   const keys1 = Object.keys(tree1);
   const keys = Object.keys(tree);
   const finishedArray = {};
+  const arrayOfKeys = ['  ', '+ ', '- '];
   _.uniq(keys.concat(keys1).sort())
     .map((item) => {
       if (keys1.includes(item)) {
         if (_.isPlainObject(tree1[item])) {
           if (tree[item]) {
-            const newItem = '  ';
-            finishedArray[item] = [newItem, newResd(tree[item], tree1[item])];
+            //const newItem = '  ';
+            finishedArray[item] = [arrayOfKeys[0], newResd(tree[item], tree1[item])];
           } else {
-            const newItem1 = '+ ';
-            finishedArray[item] = [newItem1, tree1[item]];
+            //const newItem1 = '+ ';
+            finishedArray[item] = [arrayOfKeys[1], tree1[item]];
           }
         } else if (tree[item] === tree1[item]) {
-          const newItem = '  ';
-          finishedArray[item] = [newItem, tree[item]];
+          //const newItem = '  ';
+          finishedArray[item] = [arrayOfKeys[0], tree[item]];
         } else if (keys.includes(item)) {
-          const newItem = '- ';
-          const newItem1 = '+ ';
-          finishedArray[item] = [newItem, newItem1, tree[item], tree1[item]];
+          //const newItem = '- ';
+          //const newItem1 = '+ ';
+          finishedArray[item] = [arrayOfKeys[2], arrayOfKeys[1], tree[item], tree1[item]];
         } else {
-          const newItem1 = '+ ';
-          finishedArray[item] = [newItem1, tree1[item]];
+          //const newItem1 = '+ ';
+          finishedArray[item] = [arrayOfKeys[1], tree1[item]];
         }
       } else if (_.isPlainObject(tree[item])) {
-        const newItem = '- ';
-        finishedArray[item] = [newItem, tree[item]];
+        //const newItem = '- ';
+        finishedArray[item] = [arrayOfKeys[2], tree[item]];
       } else {
-        const newItem1 = '- ';
-        finishedArray[item] = [newItem1, tree[item]];
+        //const newItem1 = '- ';
+        finishedArray[item] = [arrayOfKeys[2], tree[item]];
       }
       return finishedArray;
     });
