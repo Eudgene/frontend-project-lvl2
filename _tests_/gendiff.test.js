009224feba9da1473a85fc399460a38d64ea9d45
Eudgene <Eudgene8@mail.ru>
2021-11-14 12:37:52 +0300

    changed test4

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -48,7 +48,7 @@ const resultedForTestsOfParser = {
     setting1: 'Value 1',
     setting2: 200,
     setting3: true,
-    setting6: { key: 'value', doge: [Object] }
+    setting6: { key: 'value', doge: { "wow": "" } }
   },
   group1: { baz: 'bas', foo: 'bar', nest: { key: 'value' } },
   group2: { abc: 12345, deep: { id: 45 } }
