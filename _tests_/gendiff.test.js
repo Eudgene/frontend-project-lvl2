598311c9dcd48e762a990a98fdf3baa150db53b9
Eudgene <Eudgene8@mail.ru>
2021-11-14 11:59:27 +0300

    changed testы

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -10,32 +10,6 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const exemple = {
-  "common": {
-    "setting1": "Value 1",
-    "setting2": 200,
-    "setting3": true,
-    "setting6": {
-      "key": "value",
-      "doge": {
-        "wow": ""
-      }
-    }
-  },
-  "group1": {
-    "baz": "bas",
-    "foo": "bar",
-    "nest": {
-      "key": "value"
-    }
-  },
-  "group2": {
-    "abc": 12345,
-    "deep": {
-      "id": 45
-    }
-  }
-};
 
 const resulted = {
   common: [
