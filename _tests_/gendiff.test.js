d3a47eefff838d0c3101634e70825713c57d6b7f
Eudgene <Eudgene8@mail.ru>
2021-11-14 12:20:19 +0300

    changed tests

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -15,23 +15,23 @@ const resulted = {
   common: [
     '  ',
     {
-      "follow": ['+ ', false],
-      "setting1": ['  ', "Value 1"],
-      "setting2": ['- ', 200],
-      "setting3": ['- ', '+ ', true, null],
-      "setting4": ['+ ', "blah blah"],
-      "setting5": ['+ ', { "key5": "value5" }],
-      "setting6": [
+      follow: ['+ ', false],
+      setting1: ['  ', "Value 1"],
+      setting2: ['- ', 200],
+      setting3: ['- ', '+ ', true, null],
+      setting4: ['+ ', "blah blah"],
+      setting5: ['+ ', { "key5": "value5" }],
+      setting6: [
         '  ',
         {
-          "doge": ['  ', { "wow": ['- ', '+ ', '', "so much"] }],
-          "key": ['  ', "value"],
-          "ops": ['+ ', "vops"]
+          doge: ['  ', { "wow": ['- ', '+ ', '', "so much"] }],
+          key: ['  ', "value"],
+          ops: ['+ ', "vops"]
         }
       ]
     }
   ],
-  "group1": [
+  group1: [
     '  ',
     {
       "baz": ['- ', '+ ', "bas", "bars"],
@@ -39,8 +39,8 @@ const resulted = {
       "nest": ['- ', '+ ', { "key": "value" }, "str"]
     }
   ],
-  "group2": ['- ', { "abc": 12345, "deep": { "id": 45 } }],
-  "group3": ['+ ', { "deep": { "id": { "number": 45 } }, "fee": 100500 }]
+  group2: ['- ', { "abc": 12345, "deep": { "id": 45 } }],
+  group3: ['+ ', { "deep": { "id": { "number": 45 } }, "fee": 100500 }]
 };
 
 describe('1-Сhecking the work of readFile', () => {
