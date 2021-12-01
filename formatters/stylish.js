53bf0aed1c6b85b9d7657e4e7a52738e2b3ac2ee
Eudgene <Eudgene8@mail.ru>
2021-12-01 08:57:59 +0300

    changed stylish after linter

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -2,10 +2,10 @@ const stylish = (value, replacer = ' ', spacesCount = 1) => {
   const iter = (currentValue, depth) => {
     if (typeof currentValue !== 'object') {
       return currentValue.toString();
-    } else if (currentValue === null) {
+    }
+    if (currentValue === null) {
       return 'null';
     }
-    
     const indentSize = depth * spacesCount;
     const currentIn = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
@@ -14,24 +14,24 @@ const stylish = (value, replacer = ' ', spacesCount = 1) => {
     const lines = Object
       .entries(currentValue)
       .map(([key, val]) => {
-          const findPrefix = (item) => {
-            let prefix = '  ';
-            if (item === `added`) {
-              prefix = '+ ';
-            } else if (item === `removed`) {
-              prefix = '- ';
-            } else if (item === `updated`) {
-              prefix = ['- ', '+ '];
-            }
-            return prefix;
-          };
-          if (val.length === 2 ) {
-            bbb = `${currentIndent}${findPrefix(val[0])}${key}: ${iter(val[1], depth + 2)}`;
-          } else if (val.length === 3) {
-            bbb = `${currentIndent}${findPrefix(val[0])[0]}${key}: ${iter(val[1], depth + 2)}\n${currentIndent}${findPrefix(val[0])[1]}${key}: ${val[2]}`;
-          } else {
-            bbb = `${currentIndent}  ${key}: ${iter(val, depth + 2)}`;
+        const findPrefix = (item) => {
+          let prefix = '  ';
+          if (item === `added`) {
+            prefix = '+ ';
+          } else if (item === `removed`) {
+            prefix = '- ';
+          } else if (item === `updated`) {
+            prefix = ['- ', '+ '];
           }
+          return prefix;
+        };
+        if (val.length === 2 ) {
+          bbb = `${currentIndent}${findPrefix(val[0])}${key}: ${iter(val[1], depth + 2)}`;
+        } else if (val.length === 3) {
+          bbb = `${currentIndent}${findPrefix(val[0])[0]}${key}: ${iter(val[1], depth + 2)}\n${currentIndent}${findPrefix(val[0])[1]}${key}: ${val[2]}`;
+        } else {
+          bbb = `${currentIndent}  ${key}: ${iter(val, depth + 2)}`;
+        }
         return bbb;
       });
     return [
