4a4b066cae8ade97ac1de169853cc5513315b1c9
Eudgene <Eudgene8@mail.ru>
2021-12-03 08:19:30 +0300

    added test for plain

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -119,8 +119,6 @@ describe('4-Сhecking the Parsers', () => {
 
 describe('5-Сhecking the Plain', () => {
   test('Сhecking the result for compliance with the expected', () => {
-    console.log(resultOfPlain);
-    console.log(plain(resulted));
     expect(plain(resulted)).toEqual(resultOfPlain);
   });
 });
