ac0062af5b265601d9bbbf93b94d3c627ad53cff
Eudgene <Eudgene8@mail.ru>
2021-12-03 08:17:44 +0300

    added test for plain

--- formatters/plain.js
+++ formatters/plain.js
@@ -21,7 +21,7 @@ const plain = (value) => {
       .map(([key, val]) => {
         let newStringWay = stringWay;
         if (stringWay !== '') {
-          console.log(newStringWay.split('.'));
+          console.log(newStringWay);
           const arr = newStringWay.split('.');
           if (arr.length !== depth) {
             arr.splice(arr.length - 1, 1);
