d934e8642f430f5e64a557cfdb46f063959ed3eb
Eudgene <Eudgene8@mail.ru>
2021-12-06 08:51:36 +0300

    test chooseFormat5

--- formatters/index.js
+++ formatters/index.js
@@ -2,13 +2,11 @@ import stylish from '../formatters/stylish.js';
 import plain from '../formatters/plain.js';
 
 const chooseFormat = (base, format) => {
-    console.log(format === 'stylish');
-    
   let result;
-  if (format === 'stylish') {
-    
+  [key, value] = format;
+  if (value === 'stylish') {
     result = stylish(base, ' ', 2);
-  } else if (format === 'plain') {
+  } else if (value === 'plain') {
     result = plain(base);
   }
   return console.log(result);
