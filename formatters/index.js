b320e40cedc5c2479d6413adc0bb5925bd052f0b
Eudgene <Eudgene8@mail.ru>
2021-12-06 08:59:24 +0300

    chaged after linter

--- formatters/index.js
+++ formatters/index.js
@@ -1,11 +1,11 @@
-import stylish from '../formatters/stylish.js';
-import plain from '../formatters/plain.js';
+import stylish from '../stylish.js';
+import plain from '../plain.js';
 
 const chooseFormat = (base, format) => {
   let result;
-  if (format['format'] === 'stylish') {
+  if (format.format === 'stylish') {
     result = stylish(base, ' ', 2);
-  } else if (format['format'] === 'plain') {
+  } else if (format.format === 'plain') {
     result = plain(base);
   }
   return console.log(result);
