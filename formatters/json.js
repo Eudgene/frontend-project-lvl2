4c9daa8172834a80f016e7e3d93fa86827f3c2f0
Eudgene <Eudgene8@mail.ru>
2021-12-13 08:26:31 +0300

    changed after linter

--- formatters/json.js
+++ formatters/json.js
@@ -17,29 +17,28 @@ const toJson = (value) => {
       .map(([key, val]) => {
         if (depth <= arrDepth.pop()) {
           if (val.length === 2) {
-            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            const newArr = _.last(arrDepth) === depth ? arrDepth : arrDepth.push(depth);
             bbb = `,"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)}]`;
           } else if (val.length === 3) {
             const withkav2 = typeof val[2] === 'string' ? `"${(val[2])}"` : `${(val[2])}`;
-            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            const newArr = _.last(arrDepth) === depth ? arrDepth : arrDepth.push(depth);
             bbb = `,"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)},${withkav2}]`;
           } else {
-            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            const newArr = _.last(arrDepth) === depth ? arrDepth : arrDepth.push(depth);
             bbb = `,"${key}":${iter(val, depth + 1, newArr)}`;
           }
+        } else if (val.length === 2 ) {
+          const newArr = _.last(arrDepth) === depth ? arrDepth : arrDepth.push(depth);
+          bbb = `"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)}]`;
+        } else if (val.length === 3) {
+          const newArr = _.last(arrDepth) === depth ? arrDepth : arrDepth.push(depth);
+          const withkav2 = typeof val[2] === 'string' ? `"${(val[2])}"` : `${(val[2])}`;
+          bbb = `"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)},${withkav2}]`;
         } else {
-          if (val.length === 2 ) {
-            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
-            bbb = `"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)}]`;
-          } else if (val.length === 3) {
-            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
-            const withkav2 = typeof val[2] === 'string' ? `"${(val[2])}"` : `${(val[2])}`;
-            bbb = `"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)},${withkav2}]`;
-          } else {
-            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
-            bbb = `"${key}":${iter(val, depth + 1, newArr)}`;
-          }
+          const newArr = _.last(arrDepth) === depth ? arrDepth : arrDepth.push(depth);
+          bbb = `"${key}":${iter(val, depth + 1, newArr)}`;
         }
+
         return bbb;
       });
     return [
