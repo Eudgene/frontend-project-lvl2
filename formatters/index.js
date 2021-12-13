f27ec1bae63b09b4ca4f195d1258903bf4d0ee19
Eudgene <Eudgene8@mail.ru>
2021-12-13 08:43:21 +0300

    added json.js to logic

--- formatters/index.js
+++ formatters/index.js
@@ -1,5 +1,6 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import toJson from './json.js';
 
 const chooseFormat = (base, name) => {
   let result;
@@ -7,6 +8,8 @@ const chooseFormat = (base, name) => {
     result = stylish(base, ' ', 2);
   } else if (name.format === 'plain') {
     result = plain(base);
+  } else if (name.format === 'json') {
+    result = toJson(base);
   }
   return console.log(result);
 };
