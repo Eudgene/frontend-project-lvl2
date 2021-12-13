13a9d1db3c8ef0b68890bafc6f6505c6f5e607b7
Eudgene <Eudgene8@mail.ru>
2021-12-13 08:16:02 +0300

    added json.js

---
+++ formatters/json.js
@@ -0,0 +1,53 @@
+const toJson = (value) => {
+  const arrDepth = [0];
+  const iter = (currentValue, depth, previusDepth) => {
+    if (currentValue === null) {
+      return 'null';
+    }
+    if (typeof currentValue !== 'object') {
+      const stringVal = typeof currentValue === 'string' ? `"${currentValue.toString()}"` : `${currentValue.toString()}`;
+      return stringVal;
+    }
+      
+    let bbb = {};
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => {
+        if (depth <= arrDepth.pop()) {
+          if (val.length === 2 ) {
+            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            bbb = `,"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)}]`;
+          } else if (val.length === 3) {
+            const withkav2 = typeof val[2] === 'string' ? `"${(val[2])}"` : `${(val[2])}`;
+            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            bbb = `,"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)},${withkav2}]`;
+          } else {
+            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            bbb = `,"${key}":${iter(val, depth + 1, newArr)}`;
+          }
+        } else {
+          if (val.length === 2 ) {
+            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            bbb = `"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)}]`;
+          } else if (val.length === 3) {
+            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            const withkav2 = typeof val[2] === 'string' ? `"${(val[2])}"` : `${(val[2])}`;
+            bbb = `"${key}":["${val[0]}",${iter(val[1], depth + 1, newArr)},${withkav2}]`;
+          } else {
+            const newArr = _.last(arrDepth) === depth ? arrDepth: arrDepth.push(depth);
+            bbb = `"${key}":${iter(val, depth + 1, newArr)}`;
+          }
+        }
+        return bbb;
+      });
+    return [
+      '{',
+      ...lines,
+      `}`,
+    ].join('');
+  };
+  
+  return iter(value, 1, arrDepth);
+};
+
+export default toJson;
