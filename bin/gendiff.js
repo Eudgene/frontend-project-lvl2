32df70b91e37a90a624a6e6a2c02829f0cbefb48
Eudgene <Eudgene8@mail.ru>
2021-12-15 08:56:30 +0300

    test6

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,7 @@ import { Command } from 'commander';
 import { newResd, takeObjectFromJson } from './funcs.js';
 import chooseFormat from '../formatters/index.js';
 
-export default program = new Command();
+const program = new Command();
 program
   .description('Compares two configuration files and shows a difference')
   .version('0.0.1', '-V, --version', 'output the version number')
@@ -18,3 +18,4 @@ program
   })
   .parse();
   
+export default program;
