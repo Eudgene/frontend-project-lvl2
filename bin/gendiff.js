cae29d742db76fe715205d20c7a27432fa5320f8
Eudgene <Eudgene8@mail.ru>
2021-12-19 13:55:59 +0300

    added starter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,18 +4,20 @@ import { Command } from 'commander';
 import { newResd, takeObjectFromJson } from './funcs.js';
 import chooseFormat from '../formatters/index.js';
 
-const program = new Command();
-program
-  .description('Compares two configuration files and shows a difference')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
-  .action((filepath1, filepath2, format = 'stylish') => {
-    const json1 = takeObjectFromJson(filepath1);
-    const json2 = takeObjectFromJson(filepath2);
-    const newArr = newResd(json1, json2);
-    chooseFormat(newArr, format);
-  })
-  .parse();
-  
-export default program;
+const starter = (filepath1, filepath2) => {
+  const program = new Command();
+  program
+    .description('Compares two configuration files and shows a difference')
+    .version('0.0.1', '-V, --version', 'output the version number')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
+    .action((filepath1, filepath2, format = 'stylish') => {
+      const json1 = takeObjectFromJson(filepath1);
+      const json2 = takeObjectFromJson(filepath2);
+      const newArr = newResd(json1, json2);
+      chooseFormat(newArr, format);
+    })
+    .parse();
+};
+
+export default starter;
