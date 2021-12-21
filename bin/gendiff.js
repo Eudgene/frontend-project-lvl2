84415e8f8cd123a9599656ee15de2d23e0319bcc
Eudgene <Eudgene8@mail.ru>
2021-12-21 08:29:35 +0300

    test16

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,21 +4,6 @@ import { Command } from 'commander';
 import { takeObjectFromJson } from './funcs.js';
 import chooseFormat from '../formatters/index.js';
 
-const program = new Command();
-program
-  .description('Compares two configuration files and shows a difference')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
-  .action((filepath1, filepath2, format = 'stylish') => {
-    const json1 = takeObjectFromJson(filepath1);
-    const json2 = takeObjectFromJson(filepath2);
-    const newArr = newResd(json1, json2);
-    chooseFormat(newArr, format);
-  });
-  
-program.parse();
-
 const newResd = (tree, tree1) => {
   const keys1 = Object.keys(tree1);
   const keys = Object.keys(tree);
@@ -55,5 +40,20 @@ const newResd = (tree, tree1) => {
     });
   return finishedArray;
 };
+
+const program = new Command();
+program
+  .description('Compares two configuration files and shows a difference')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
+  .action((filepath1, filepath2, format = 'stylish') => {
+    const json1 = takeObjectFromJson(filepath1);
+    const json2 = takeObjectFromJson(filepath2);
+    const newArr = newResd(json1, json2);
+    chooseFormat(newArr, format);
+  });
+  
+program.parse();
 
 export default newResd;
