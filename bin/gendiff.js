1ff1d83888482d1019f0fa2c089e59bc1969a983
Eudgene <Eudgene8@mail.ru>
2021-12-21 08:43:55 +0300

    test20

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -42,11 +42,13 @@ const newResd = (tree, tree1) => {
   return finishedArray;
 };
 
+function gendiff(path1, path2) {
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
+  .arguments('<filepath1>', 'argument', path1)
+  .arguments('<filepath2>', 'argument', path2)
   .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
   .action((filepath1, filepath2, format = 'stylish') => {
     const json1 = takeObjectFromJson(filepath1);
@@ -56,5 +58,5 @@ program
   });
   
 program.parse();
-
-export default newResd;
+};
+export default gendiff;
