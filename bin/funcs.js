d294ae582f2e38ef6e6c614110b5444bf4c9b4ae
Eudgene <Eudgene8@mail.ru>
2021-12-22 08:24:08 +0300

    test30

--- bin/funcs.js
+++ bin/funcs.js
@@ -3,10 +3,6 @@ import * as path from 'path';
 import _ from 'lodash';
 import parsFunc from './parsers.js';
 
-//import { Command } from 'commander';
-//import chooseFormat from '../formatters/index.js';
-//import { newResd } from './gendiff.js';
-
 export const takeObjectFromJson = (file) => {
   const filePath = path.isAbsolute(file) ? file : path.resolve(process.cwd(), file);
   const read = fs.readFileSync(`${filePath}`, 'utf8');
@@ -50,17 +46,3 @@ export const newResd = (tree, tree1) => {
     });
   return finishedArray;
 };
-
-/*const program = new Command();
-program
-  .description('Compares two configuration files and shows a difference')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
-  .action((filepath1, filepath2, format = 'stylish') => {
-    const json1 = takeObjectFromJson(filepath1);
-    const json2 = takeObjectFromJson(filepath2);
-    const newArr = newResd(json1, json2);
-    chooseFormat(newArr, format);
-  })
-  .parse(process.argv);*/
