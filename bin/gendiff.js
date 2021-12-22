9824845e7c943f6e38559b9cbfe101388dadde2f
Eudgene <Eudgene8@mail.ru>
2021-12-22 08:24:42 +0300

    test31

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,43 +5,6 @@ import { takeObjectFromJson } from './funcs.js';
 import chooseFormat from '../formatters/index.js';
 import _ from 'lodash';
 
-/*const newResd = (tree, tree1) => {
-  const keys1 = Object.keys(tree1);
-  const keys = Object.keys(tree);
-  const finishedArray = {};
-  _.uniq(keys.concat(keys1).sort())
-    .map((item) => {
-      if (keys1.includes(item)) {
-        if (_.isPlainObject(tree1[item])) {
-          if (tree[item]) {
-            const newItem = 'notChanged';
-            finishedArray[item] = [newItem, newResd(tree[item], tree1[item])];
-          } else {
-            const newItem1 = 'added';
-            finishedArray[item] = [newItem1, tree1[item]];
-          }
-        } else if (tree[item] === tree1[item]) {
-          const newItem = 'notChanged';
-          finishedArray[item] = [newItem, tree[item]];
-        } else if (keys.includes(item)) {
-          const newItem = 'updated';
-          finishedArray[item] = [newItem, tree[item], tree1[item]];
-        } else {
-          const newItem1 = 'added';
-          finishedArray[item] = [newItem1, tree1[item]];
-        }
-      } else if (_.isPlainObject(tree[item])) {
-        const newItem = 'removed';
-        finishedArray[item] = [newItem, tree[item]];
-      } else {
-        const newItem1 = 'removed';
-        finishedArray[item] = [newItem1, tree[item]];
-      }
-      return finishedArray;
-    });
-  return finishedArray;
-};*/
-
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference')
@@ -56,5 +19,3 @@ program
   });
   
 program.parse();
-
-//export default newResd;
