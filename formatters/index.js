64b3d1b8efe98556474a4f7d8d8ac4fff97c7458
Eudgene <Eudgene8@mail.ru>
2022-01-07 12:12:03 +0300

    changed index.js after tests

--- formatters/index.js
+++ formatters/index.js
@@ -3,13 +3,13 @@ import plain from './plain.js';
 import toJson from './json.js';
 
 const chooseFormat = (base, name) => {
-  let result = stylish(base, ' ', 2);
   if (name === 'plain') {
-    result = plain(base);
-  } else if (name === 'json') {
-    result = toJson(base);
+    return plain(base);
   }
-  return result;
+  if (name === 'json') {
+    return toJson(base);
+  }
+  return stylish(base, ' ', 2);;
 };
 
 export default chooseFormat;
