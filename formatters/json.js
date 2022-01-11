05a2875e0fc653d132541931b2598e40b9689d20
Eudgene <Eudgene8@mail.ru>
2022-01-11 21:35:21 +0300

    changed json.js delete mutation

--- formatters/json.js
+++ formatters/json.js
@@ -16,29 +16,24 @@ const toJson = (value) => {
         if (depth <= _.last(arrDepth)) {
           arrDepth.push(depth);
           if (val.length === 2) {
-            //arrDepth.push(depth);
             return `,"${key}":["${val[0]}",${iter(val[1], depth + 1, arrDepth)}]`;
           }
           if (val.length === 3) {
-            //arrDepth.push(depth);
             return `,"${key}":["${val[0]}",${iter(val[1], depth + 1, arrDepth)},${iter(val[2], depth + 1, arrDepth)}]`;
           }
           if (val.length !== 3 && val.length !== 2) {
-            //arrDepth.push(depth);
             return `,"${key}":${iter(val, depth + 1, arrDepth)}`;
           }
         }
+        arrDepth.push(depth);
         if (val.length === 2) {
-          arrDepth.push(depth);
           return `"${key}":["${val[0]}",${iter(val[1], depth + 1, arrDepth)}]`;
         }
         if (val.length === 3) {
-          arrDepth.push(depth);
           const withkav2 = typeof val[2] === 'string' ? `"${(val[2])}"` : `${(val[2])}`;
           return `"${key}":["${val[0]}",${iter(val[1], depth + 1, arrDepth)},${withkav2}]`;
         }
         if (val.length !== 3 && val.length !== 2) {
-          arrDepth.push(depth);
           return `"${key}":${iter(val, depth + 1, arrDepth)}`;
         }
         return '';
