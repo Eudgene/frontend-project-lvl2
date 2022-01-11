a844220afa700541892529d96335b781e76b4931
Eudgene <Eudgene8@mail.ru>
2022-01-11 21:29:56 +0300

    changed json.js delete mutation

--- formatters/json.js
+++ formatters/json.js
@@ -17,29 +17,35 @@ const toJson = (value) => {
           //arrDepth.concat(depth);
           if (val.length === 2) {
             //arrDepth.push(depth);
-            return `,"${key}":["${val[0]}",${iter(val[1], depth + 1, depth)}]`;
+            const newDepth = depth;
+            return `,"${key}":["${val[0]}",${iter(val[1], depth + 1, newDepth)}]`;
           }
           if (val.length === 3) {
             //arrDepth.push(depth);
-            return `,"${key}":["${val[0]}",${iter(val[1], depth + 1, depth)},${iter(val[2], depth + 1, depth)}]`;
+            const newDepth = depth;
+            return `,"${key}":["${val[0]}",${iter(val[1], depth + 1, newDepth)},${iter(val[2], depth + 1, newDepth)}]`;
           }
           if (val.length !== 3 && val.length !== 2) {
             //arrDepth.push(depth);
-            return `,"${key}":${iter(val, depth + 1, depth)}`;
+            const newDepth = depth;
+            return `,"${key}":${iter(val, depth + 1, newDepth)}`;
           }
         }
         if (val.length === 2) {
           //arrDepth.push(depth);
-          return `"${key}":["${val[0]}",${iter(val[1], depth + 1, depth)}]`;
+          const newDepth = depth;
+          return `"${key}":["${val[0]}",${iter(val[1], depth + 1, newDepth)}]`;
         }
         if (val.length === 3) {
           //arrDepth.push(depth);
+          const newDepth = depth;
           const withkav2 = typeof val[2] === 'string' ? `"${(val[2])}"` : `${(val[2])}`;
-          return `"${key}":["${val[0]}",${iter(val[1], depth + 1, depth)},${withkav2}]`;
+          return `"${key}":["${val[0]}",${iter(val[1], depth + 1, newDepth)},${withkav2}]`;
         }
         if (val.length !== 3 && val.length !== 2) {
           //arrDepth.push(depth);
-          return `"${key}":${iter(val, depth + 1, depth)}`;
+          const newDepth = depth;
+          return `"${key}":${iter(val, depth + 1, newDepth)}`;
         }
         return '';
       });
