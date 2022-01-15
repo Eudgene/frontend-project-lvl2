4416002521ed42a75db3cf96ba0ed18028806dfe
Eudgene <Eudgene8@mail.ru>
2022-01-15 10:01:31 +0300

    added commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
-import { newResd } from './funcs.js';
-import chooseFormat from '../formatters/index.js';
+import { Command } from 'commander';
+import gendiff from './funcs.js';
 
-const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const preResalt = newResd(filepath1, filepath2);
-  return chooseFormat(preResalt, format);
-};
+const program = new Command();
+program
+  .description('Compares two configuration files and shows a difference')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish', 'stylish')
+  .action((filepath1, filepath2, format = 'stylish') => {
+    console.log(gendiff(filepath1, filepath2, format.format));
+  });
 
-export default gendiff;
+program.parse();
