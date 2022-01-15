b183c11a545e4a605cff467cdd7074286955ad18
Eudgene <Eudgene8@mail.ru>
2022-01-15 09:57:06 +0300

    added commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import newResd from './funcs.js';
+import { newResd } from './funcs.js';
 import chooseFormat from '../formatters/index.js';
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
