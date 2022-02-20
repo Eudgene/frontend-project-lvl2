f5ff719f46c0555e764ff5cc1b0cde7ea90b74c4
Eudgene <Eudgene8@mail.ru>
2022-02-20 12:38:59 +0300

    changed stilysh.js

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,6 +1,60 @@
 const stylish = (value, replacer = ' ', spacesCount = 1) => {
   const iter = (currentValue, depth) => {
     if (typeof currentValue !== 'object') {
+      if (currentValue === undefined) {
+      return 'undefined';
+      }
+      return currentValue.toString();
+    }
+    if (currentValue === null) {
+      return 'null';
+    }
+    //console.log(currentValue)
+    const indentSize = depth * spacesCount;
+    const currentIn = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const currentIndent = currentIn === undefined ? '' : currentIn;
+    const lines = currentValue.map(({item, prefix, value, value2}) => {
+          const findPrefix = (item) => {
+            if (item === 'added') {
+              return '+ ';
+            }
+            if (item === 'removed') {
+              return '- ';
+            }
+            if (item === 'updated') {
+              return ['- ', '+ '];
+            }
+            return '  ';
+          };
+          if (typeof value === 'object') {
+            
+            if (value2 !== undefined) {
+              
+              return `${currentIndent}${findPrefix(prefix)[0]}${item}: ${iter(value, depth + 2)}\n${currentIndent}${findPrefix(prefix)[1]}${item}: ${iter(value2, depth + 2)}`;
+            }
+            
+            return `${currentIndent}${findPrefix(prefix)}${item}: ${iter(value, depth + 2)}`;
+          } else {
+            
+            if (value2 !== undefined) {
+              return `${currentIndent}${findPrefix(prefix)[0]}${item}: ${iter(value, depth + 2)}\n${currentIndent}${findPrefix(prefix)[1]}${item}: ${iter(value2, depth + 2)}`;
+            }
+            const val1 = typeof value === 'object' ? value : iter(value, depth + 2);
+            return `${currentIndent}${findPrefix(prefix)}${item}: ${val1}`;
+          }
+        return ' ';
+      });
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(value, 1);
+  /*const iter = (currentValue, depth) => {
+    if (typeof currentValue !== 'object') {
       return currentValue.toString();
     }
     if (currentValue === null) {
@@ -45,7 +99,7 @@ const stylish = (value, replacer = ' ', spacesCount = 1) => {
     ].join('\n');
   };
 
-  return iter(value, 1);
+  return iter(value, 1);*/
 };
 
 export default stylish;
