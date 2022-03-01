3e1d5976fd0875670831bc8838b37b26b2aae65e
Eudgene <Eudgene8@mail.ru>
2022-03-01 08:39:07 +0300

    changed plain.js

--- formatters/plain.js
+++ formatters/plain.js
@@ -13,9 +13,9 @@ const valueInString = (value) => {
   return value;
 };
 
-const plain = (value) => {
+const plain = (val) => {
   const iter = (currentValue, stringWay = '', depth = 0) => {
-    const lines = currentValue.map(({item, prefix, value, value2}) => {
+    const lines = currentValue.map(({ item, prefix, value, value2 }) => {
         const findPrefix = (way) => {
           if (way !== '') {
             const arr = way.split('.');
@@ -54,50 +54,7 @@ const plain = (value) => {
     ].filter(Boolean).join('\n').trim();
   };
 
-  return iter(value);
-  /*const iter = (currentValue, stringWay = '', depth = 0) => {
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => {
-        const findPrefix = (way) => {
-          if (way !== '') {
-            const arr = way.split('.');
-            if (arr.length !== depth) {
-              _.slice(arr, 0, arr.length - 1);
-              return arr.join('.');
-            }
-          }
-          return way;
-        };
-        const prefixWay = findPrefix(stringWay);
-        const wayInString = prefixWay === '' ? key : `.${key}`;
-        if (val[0] === 'added') {
-          return `Property '${prefixWay}${wayInString}' was added with value: ${valueInString(val[1])}`;
-        }
-        if (val[0] === 'removed') {
-          return `Property '${prefixWay}${wayInString}' was removed`;
-        }
-        if (val[0] === 'updated') {
-          return `Property '${prefixWay}${wayInString}' was updated. From ${valueInString(val[1])} to ${valueInString(val[2])}`;
-        }
-        if (val[0] === 'notChanged') {
-          const prefix = findPrefix(stringWay);
-          const newPrefix = `${prefix}.${key}`;
-          const newString = prefix === '' ? key : newPrefix;
-          if (typeof val[1] === 'object') {
-            return iter(val[1], newString, depth + 1);
-          }
-          return iter(val[1], newString, depth + 1);
-        }
-        return ' ';
-      });
-
-    return [
-      ...lines,
-    ].filter(Boolean).join('\n').trim();
-  };
-
-  return iter(value);*/
+  return iter(val);
 };
 
 export default plain;
