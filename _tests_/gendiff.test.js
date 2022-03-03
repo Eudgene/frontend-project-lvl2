4cd7a5fd32dfdb80263ebfe3f1214c6276568daa
Eudgene <Eudgene8@mail.ru>
2022-03-03 18:34:14 +0300

    changed tests

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -21,33 +21,51 @@ const resulted = [
       { item: 'follow', prefix: 'added', value: false },
       { item: 'setting1', prefix: 'notChanged', value: 'Value 1' },
       { item: 'setting2', prefix: 'removed', value: 200 },
-      { item: 'setting3', prefix: 'updated', value: true, value2: null },
+      {
+        item: 'setting3', prefix: 'updated', value: true, value2: null
+      },
       { item: 'setting4', prefix: 'added', value: 'blah blah' },
       { item: 'setting5', prefix: 'added', value: [{ item: 'key5', prefix: '', value: 'value5' }] },
-      { item: 'setting6', prefix: 'notChanged', value: [
-        { item: 'doge', prefix: 'notChanged', value: [{ item: 'wow', prefix: 'updated', value: '', value2: 'so much' }] },
+      {
+        item: 'setting6',
+        prefix: 'notChanged',
+        value: [
+        {
+          item: 'doge', prefix: 'notChanged', value: [{ item: 'wow', prefix: 'updated', value: '', value2: 'so much' }]
+        },
         { item: 'key', prefix: 'notChanged', value: 'value' },
-        { item: 'ops', prefix: 'added', value: 'vops' }
-      ] }
-    ]
+        { item: 'ops', prefix: 'added', value: 'vops' },
+      ]
+      },
+    ],
   },
   {
     item: 'group1',
     prefix: 'notChanged',
     value: [
-      { item: 'baz', prefix: 'updated', value: 'bas', value2: 'bars' },
+      {
+        item: 'baz', prefix: 'updated', value: 'bas', value2: 'bars',
+      },
       { item: 'foo', prefix: 'notChanged', value: 'bar' },
-      { item: 'nest', prefix: 'updated', value: [{ item: 'key', prefix: '', value: 'value' }], value2: 'str' }
-    ]
+      {
+        item: 'nest', prefix: 'updated', value: [{ item: 'key', prefix: '', value: 'value' }], value2: 'str',
+      },
+    ],
   },
-  { item: 'group2', prefix: 'removed', value: [
-    { item: 'abc', prefix: '', value: 12345 },
-    { item: 'deep', prefix: '', value: [{ item: 'id', prefix: '', value: 45 }] }
-    ]
+  {
+    item: 'group2',
+    prefix: 'removed',
+    value: [
+      { item: 'abc', prefix: '', value: 12345 },
+      { item: 'deep', prefix: '', value: [{ item: 'id', prefix: '', value: 45 }] },
+    ],
   },
-  { item: 'group3', prefix: 'added', value: [
-    { item: 'deep', prefix: '', value: [{ item: 'id', prefix: '', value: [Array] }] },
-    { item: 'fee', prefix: '', value: 100500 }
+  {
+    item: 'group3',
+    prefix: 'added',
+    value: [
+      { item: 'deep', prefix: '', value: [{ item: 'id', prefix: '', value: [Array] }] },
+      { item: 'fee', prefix: '', value: 100500 },
     ],
   },
 ];
