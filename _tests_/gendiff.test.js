a861d7e17968de42f6960b07cbc018824894fac0
Eudgene <Eudgene8@mail.ru>
2022-03-18 08:37:11 +0300

    added test for index.js - stylish

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -77,6 +77,51 @@ const resulted = [
   },
 ];
 
+const resultOfStylish = {
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+};
+
 const resultedForTestsOfParser = {
   common: {
     setting1: 'Value 1',
@@ -180,5 +225,6 @@ describe('7-Сhecking index.js', () => {
   test('Сhecking the result for compliance with the expected', () => {
     expect(chooseFormat(resulted, 'plain')).toEqual(resultOfPlain.join('\n'));
     expect(chooseFormat(resulted, 'json')).toEqual(resultInJson);
+    expect(chooseFormat(resulted)).toEqual(resultOfStylish);
   });
 });
