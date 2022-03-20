42fb45867103decd60fa7234239c34b73fa0e670
Eudgene <Eudgene8@mail.ru>
2022-03-20 12:48:05 +0300

    chacking test for index.js on resalt from stylish

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -77,11 +77,7 @@ const resulted = [
   },
 ];
 
-const resultedOfStylish = "{"
-  "common: {"
-    "+ follow: false"
-    "  setting1: Value 1"
-    "- setting2: 200"
+const resultedOfStylish = '{  "common": {  "+ follow: false,   "  setting1: Value 1",  "- setting2: 200"
     "- setting3: true"
     "+ setting3: null"
     "+ setting4: blah blah"
